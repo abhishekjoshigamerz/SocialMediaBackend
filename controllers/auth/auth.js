acb98f7dc25020b4f5a799ebeb19eca8b4dc84d8
abhishekjoshigamerz <abhishekjoshicctian1@gmail.com>
2023-03-03 22:04:50 +0530

    User schema and models

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,5 +1,33 @@
 const User = require('../../models/user/user');
 
-module.exports.register = function(req,res){
-    const user = new User(req.body);
+module.exports.registerPage = async function(req,res){
+   
+}
+
+
+//registration
+module.exports.register = async function(req,res){
+    try {
+        const user = User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password:req.body.password
+        });
+        if(user){
+            return res.json({
+                "Message":"User Registered"
+            })
+        }else{
+    
+            return res.json({
+                "Message":"User  Registaration Failed"
+            })
+        }    
+    } catch (error) {
+        res.status(500).json({
+            "Message":"Internal Server Error"
+        })
+    }
+    
+
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../../controllers/auth/auth');
 
+router.get('/register',authController.registerPage);
 router.post('/register',authController.register);
 
 
